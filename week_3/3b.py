4225373403e69d87f97862f1a62df2abe7e90902
sushma <sushma.k@anko.com>
2023-04-10 16:38:52 +0530

    added files1

--- week_3/3b.py
+++ week_3/3b.py
@@ -1,47 +1,30 @@
+import snowflake.connector
+import os
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-# Load data into a pandas dataframe
-data = pd.read_csv('C:/Users/ssushma/OneDrive - Kmart Australia Limited/KHome/Downloads/FINAL_MON_FEB.csv')
-
-# Group data by RBU_DESCRIPTION and sum the TOTAL_QUANTITY_SOLD
-grouped_data = data.groupby('RBU_DESCRIPTION')['TOTAL_QUANTITY_SOLD'].sum().reset_index()
-
-# Sort data by the total quantity sold in descending order
-grouped_data = grouped_data.sort_values('TOTAL_QUANTITY_SOLD', ascending=False)
-
-# Set a custom color palette
-custom_palette = ['#009B9E', '#FFC75F', '#FF5F5F', '#8E54E9', '#4776E6', '#53BBF4' ,'#19376D', '#FFB4B4', '#E11299', '#867070']
-
-# Create a horizontal bar chart with a logarithmic scale and custom color palette
-sns.barplot(x='TOTAL_QUANTITY_SOLD', y='RBU_DESCRIPTION', data=grouped_data, log=True, palette=custom_palette)
-
-# Set chart title and axis labels
-plt.title('Total Quantity Sold by RBU Description', fontweight='bold', fontsize=14)
-plt.xlabel('Total Quantity Sold (log scale)', fontweight='bold', fontsize=12)
-plt.ylabel('RBU Description', fontweight='bold', fontsize=12)
-
-# Add annotations to the plot
-for i, v in enumerate(grouped_data['TOTAL_QUANTITY_SOLD']):
-    plt.text(v, i, str(int(v/1000000)) + 'M', color='white', ha='left', va='center', fontweight='bold')  #total quantity is converted into millions, x-coordinate (v), y-coordinate (i),
-
-    # Add product names to the right side of the plot
-    product_names = data[data['RBU_DESCRIPTION'] == grouped_data.iloc[i]['RBU_DESCRIPTION']]['PRODUCT_NAME'].unique()
-    plt.text(1.05, i, '\n'.join(product_names), transform=plt.gca().get_yaxis_transform(), fontsize=10, color='gray')
-
-# Set background color and add a texture
-ax = plt.gca()   #This retrieves the current axes of the plot.
-ax.set_facecolor('#F7F7F7')   #his sets the background color of the plot to a light gray color.
-ax.grid(False)   #this turns off the gridlines on the plot.
-ax.set_axisbelow(True)    #This sets the axis ticks and labels to appear below the plotted data.
-ax.yaxis.grid(color='gray', linestyle='dashed', alpha=0.2)  #This adds dashed gray gridlines to the y-axis with a transparency of 0.2.
-ax.xaxis.grid(color='gray', linestyle='dashed', alpha=0.2)  #his adds dashed gray gridlines to the x-axis with a transparency of 0.2.
-ax.patch.set_alpha(0.5)   #This sets the alpha (transparency) of the plot background to 0.5.
-
-# Use a custom font
-plt.rcParams['font.family'] = 'sans-serif'
-plt.rcParams['font.sans-serif'] = ['Montserrat']
-
-# Show the chart
-plt.show()
+conn = snowflake.connector.connect(
+                user='sushma.k@anko.com',
+                account='kmartau.ap-southeast-2.privatelink',
+                warehouse='KSF_DATA_SCIENTIST_WH',
+                database='DATABASE',
+                schema='SCHEMA',
+                authenticator='externalbrowser'
+                )
+cur = conn.cursor()
+# Execute SQL statement
+query=cur.execute("""SELECT t1.rbu_description, t1.product_code, t1.product_name, t1.month, t1.total_quantity_sold
+FROM (
+  SELECT RBU_DESCRIPTION, PRODUCT_CODE, PRODUCT_NAME, MONTH(transaction_timestamp) AS month, SUM(QUANTITY_SOLD) AS TOTAL_QUANTITY_SOLD,
+    ROW_NUMBER() OVER (PARTITION BY RBU_DESCRIPTION, MONTH(transaction_timestamp) ORDER BY SUM(QUANTITY_SOLD) DESC) AS rn
+  FROM KSF_SOPHIA_DATA_INTELLIGENCE_HUB_PROD.SALES.FACT_SALES_DETAIL s
+  JOIN KSF_SOPHIA_DATA_INTELLIGENCE_HUB_PROD.COMMON_DIMENSIONS.DIM_PRODUCT p
+  ON s.FK_PRODUCT_ID = p.SK_PRODUCT_ID
+  WHERE YEAR(transaction_timestamp) = 2022
+  GROUP BY RBU_DESCRIPTION, PRODUCT_CODE, PRODUCT_NAME, MONTH(transaction_timestamp)
+) t1
+WHERE t1.rn = 1
+ORDER BY t1.month, t1.total_quantity_sold DESC;
+""")
+# Fetch result
+df = cur.fetch_pandas_all()
+df
